5efc88f9d911279547264f82b82eba0babb11c09
Mahmoud Saber <msalaheldin@kn-it.com>
2024-05-20 18:05:25 +0300

    new theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
 
+import 'theme/custom_colors_extension.dart';
+import 'theme/tw_theme.dart';
+
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+final ValueNotifier<ThemeData> _notifier = ValueNotifier(TWTheme.lightTheme);
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ValueListenableBuilder<ThemeData>(
+      valueListenable: _notifier,
+      builder: (_, theme, __) {
+        return MaterialApp(
+          theme: theme,
+          darkTheme: TWTheme.darkTheme,
+          home: const HomeScreen(),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _notifier.dispose();
+    super.dispose();
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Theme'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: DropdownMenu<ThemeData>(
+              initialSelection: _notifier.value,
+              onSelected: (value) {
+                _notifier.value = value!;
+              },
+              dropdownMenuEntries: [
+                DropdownMenuEntry(
+                    leadingIcon: Icon(
+                      Icons.color_lens,
+                      color: TWTheme.lightTheme
+                          .extension<CustomColorsExtension>()!
+                          .twPrimaryColorOne,
+                    ),
+                    label: 'lightTheme',
+                    value: TWTheme.lightTheme),
+                DropdownMenuEntry(
+                    leadingIcon: Icon(
+                      Icons.color_lens,
+                      color: TWTheme.darkTheme
+                          .extension<CustomColorsExtension>()!
+                          .twPrimaryColorOne,
+                    ),
+                    label: 'darkTheme',
+                    value: TWTheme.darkTheme),
+                DropdownMenuEntry(
+                    leadingIcon: Icon(
+                      Icons.color_lens,
+                      color: TWTheme.nationalDayTheme
+                          .extension<CustomColorsExtension>()!
+                          .twPrimaryColorOne,
+                    ),
+                    label: 'nationalDayTheme',
+                    value: TWTheme.nationalDayTheme),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
