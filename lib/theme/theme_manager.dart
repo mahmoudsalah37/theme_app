abff580dc3f46c290ff772d60b55d30b07b5cc00
Mahmoud Saber <mahmoudsalah37@gmail.com>
2024-05-28 21:11:12 +0300

    Refactor ThemeManager initTheme method and _setColorsModelInExtensionsTheme function
    The commit refactors the initTheme method and _setColorsModelInExtensionsTheme function in the ThemeManager class to improve code readability and maintainability. The changes include adding line breaks, formatting parameters, and renaming variables.

--- lib/theme/theme_manager.dart
+++ lib/theme/theme_manager.dart
@@ -11,23 +11,35 @@ import 'custom_colors_extension.dart';
 class ThemeManager {
   static const instance = ThemeManager._();
   const ThemeManager._();
+
   Future<void> initTheme() async {
     final lightJson = await _getFileJson(Assets.themes.lightTeme);
     final darkJson = await _getFileJson(Assets.themes.darkTheme);
     TWTheme.lightTheme = _setColorsModelInExtensionsTheme(
-        theme: TWTheme.lightTheme, json: lightJson);
+      theme: TWTheme.lightTheme,
+      json: lightJson,
+    );
     TWTheme.darkTheme = _setColorsModelInExtensionsTheme(
-        theme: TWTheme.darkTheme, json: darkJson);
+      theme: TWTheme.darkTheme,
+      json: darkJson,
+    );
   }
 
-  ThemeData _setColorsModelInExtensionsTheme(
-      {required ThemeData theme, required Map<String, dynamic> json}) {
-    return theme.copyWith(extensions: [
-      CustomColorsExtension(colorsModel: ColorsModel.fromJson(json))
-    ]);
+  ThemeData _setColorsModelInExtensionsTheme({
+    required ThemeData theme,
+    required Map<String, dynamic> json,
+  }) {
+    return theme.copyWith(
+      extensions: [
+        CustomColorsExtension(
+          colorsModel: ColorsModel.fromJson(json),
+        ),
+      ],
+    );
   }
 
   Future<Map<String, dynamic>> _getFileJson(String path) async {
-    return json.decode(await rootBundle.loadString(path));
+    final jsonString = await rootBundle.loadString(path);
+    return json.decode(jsonString);
   }
 }
